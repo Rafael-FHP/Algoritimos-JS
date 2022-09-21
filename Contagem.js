1a5b621759214f7233018f785537bd2a3001d71d
Rafael Holanda <113954195+Rafael-FHP@users.noreply.github.com>
2022-09-20 23:57:25 -0300

    Update Contagem.js

--- Contagem.js
+++ Contagem.js
@@ -1,4 +1,20 @@
-var n = parseInt(prompt("Qual o número?"));
-for(var i = 0;i <= n;i++){
-    console.log(i);
+var N = parseInt(prompt("Qual o valor de N?"));
+var cont = 0;
+var cont2 = 0;
+
+for(let i = 0;i<N;i++) {
+    var X = parseInt(prompt("Digite um número"))
+    if(X%1===0) {
+        contador++;
+    } if(X>=1) {
+        contador++;
+    } if(X<=N) {
+        contador++;
+    }
+    if(contador==3) {
+        cont2++;
+        cont = 0;
+    }
 }
+
+console.log("Foram digitados " + cont2 + " inteiros");
